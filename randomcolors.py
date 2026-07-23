81edf48407abfcd1c6939397ee289dd76b8a035f
XDeader <pinterestamirhjm@gmail.com>
2026-07-23 23:34:38 +0330

    Added new colors and a new death message + a new win system
    I added new colors with a new death message and a new win system. The win system does not end the game though.

--- randomcolors.py
+++ randomcolors.py
@@ -1,16 +1,21 @@
 # Random Color CRASH Infinity
 
+coins=0
 import random
-color=["Red","Yellow","Green","Blue","Orange","Purple"]
+color=["Red","Yellow","Green","Blue","Orange","Purple","Aqua","Lime","White","Light Blue"]
 while True:
     colors=random.choice(color)
-    predict=input("Predict the next color.")
+    predict=input("Predict the next color. ")
     if predict == colors:
-        print("Good job!")
+        print("Good Job!")
         print("The color was:", colors)
     else:
-       print("Choose again!")
+       print("Nope! Choose Again!")
        print("The color was:", colors)
     if colors == "Red":
-        print("You lost!")
+        print("You lost! You got Red.")
         break
+    elif colors == "Light Blue":
+        print("You got 1 coin.")
+        coins = 1 + coins
+        print("You have",coins,"coins, way to go!")
