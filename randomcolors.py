f0768b13225c5391b45fbcb0c86704c9af3652d8
XDeader <pinterestamirhjm@gmail.com>
2026-07-28 19:05:04 +0330

    Used the pickle library for V1.2's New Coin Saving system
    For V1.2 I used the pickle library to be able to make a saving system for the coins. I now have to put an EMPTY coins.dat file inside the files.

--- randomcolors.py
+++ randomcolors.py
@@ -2,20 +2,26 @@
 
 coins=0
 import random
+import pickle
 color=["Red","Yellow","Green","Blue","Orange","Purple","Aqua","Lime","White","Light Blue"]
 while True:
     colors=random.choice(color)
+    with open("coins.dat", "wb") as file:
+        pickle.dump(coins, file)
+    with open("coins.dat", "rb") as file:
+        coins = pickle.load(file)
     predict=input("Predict the next color. ")
     if predict == colors:
         print("Good Job!")
-        print("The color was:", colors)
+        print(f"The color was: {colors})
     else:
        print("Nope! Choose Again!")
-       print("The color was:", colors)
+       print(f"The color was: {colors})
     if colors == "Red":
         print("You lost! You got Red.")
         break
     elif colors == "Light Blue":
         print("You got 1 coin.")
-        coins = 1 + coins
-        print("You have",coins,"coins, way to go!")
+        coins = 1 += coins
+        print(f"You have {coins} coin(s), way to go!")
+    
